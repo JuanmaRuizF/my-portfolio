3757a6c8d2e41f083d46d9eb94315a1d9e98efa7
JuanmaRuizF <juanmaruizf@gmail.com>
2022-05-10 16:05:15 +0100

    Adding Location Advisor project to portfolio

--- src/Components/ProjectsMenu.js
+++ src/Components/ProjectsMenu.js
@@ -14,6 +14,7 @@ import {
   SiFirebase,
   SiTensorflow,
   SiReactrouter,
+  SiLeaflet,
 } from "react-icons/si";
 import { makeStyles } from "@material-ui/core/styles";
 import { useState } from "react";
@@ -52,6 +53,7 @@ function ProjectsMenu(props) {
     SiFirebase: SiFirebase,
     SiTensorflow: SiTensorflow,
     SiReactrouter: SiReactrouter,
+    SiLeaflet: SiLeaflet,
   };
   var IconComponent = components["DiPython"];
 
